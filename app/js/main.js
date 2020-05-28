e687d03cb08865adfc11dcccb51054d84a8c8073
vlad-secondary <vlad.povelitsa@gmail.com>
2020-05-28 04:05:58 +0300

    starter kit created

--- app/js/main.js
+++ app/js/main.js
@@ -1,35 +1,3 @@
-$('.popular_slider').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  centerMode: true,
-  autoplay: true,
-  autoplaySpeed: 2000,
-  arrows:false,
-  dots: false,
-  responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 1,
- 		  variableWidth: false,
- 		  centerMode: false,
-        }
-      },
-    ]
-});
-
-$('.offer').slick({
-  infinite: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: false,
-  autoplaySpeed: 2000,
-  arrows:false,
-  dots: true,
-});
-
-
 var sections = document.querySelectorAll('.section');
 var offset = innerHeight * 0.9;
 function setAnimation() {
